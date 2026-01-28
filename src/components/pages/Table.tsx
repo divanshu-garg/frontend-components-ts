601549f260336289f62acfb2abcb6b848d350e5d
Divanshu Garg <divanshugarg49@gmail.com>
2026-01-28 17:59:07 +0530

    added row selection, sorting state selection: everything will happen later on via backend api call

--- src/components/pages/Table.tsx
+++ src/components/pages/Table.tsx
@@ -1,7 +1,11 @@
+// selection of row: console logs when updated. no useEffect here when action to perform via this it will happen via a button
+// sorting state: will later on apply useEffect for api call when state is changed
+
 // import React from 'react'
-import { createColumnHelper, flexRender, getCoreRowModel, useReactTable } from "@tanstack/react-table"
+import { createColumnHelper, flexRender, getCoreRowModel, type SortingState, useReactTable } from "@tanstack/react-table"
 import { tableData, type TableData } from "../../utils/helper.ts"
 import React, { useState } from "react";
+import { ChevronDown, ChevronsUpDown, ChevronUp } from "lucide-react";
 
 const columnHelper = createColumnHelper<TableData>()
 
@@ -95,14 +99,31 @@ const columns = [
 ]
 
 const Table = () => {
+    // useEffect for sorting api call to be added here
+
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const [data, _setData] = useState<TableData[]>(tableData)
+    const [sorting, setSorting] = useState<SortingState>([])
+    const [rowSelection, setRowSelection] = useState({})
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const [isLoading, _setIsLoading] = useState(false);
     const rerender = React.useReducer(() => ({}), {})[1]
     const table = useReactTable({
         data,
         columns,
-        getCoreRowModel: getCoreRowModel()
+        state: {
+            sorting,
+            rowSelection,
+        },
+        getRowId: (row) => row.id,
+        getCoreRowModel: getCoreRowModel(),
+        onSortingChange: setSorting,
+        onRowSelectionChange: setRowSelection,
+        enableRowSelection: true,
+        manualSorting: true,
     })
+
+    console.log("Selected Row IDs:", rowSelection);
     return (
         <div className="p-2"> {/* Logic container */}
             <div className="overflow-x-auto border rounded-lg shadow-sm">
@@ -110,22 +131,59 @@ const Table = () => {
                     <thead className="text-xs text-gray-700 uppercase bg-gray-50">
                         {table.getHeaderGroups().map((headerGroup) => (
                             <tr key={headerGroup.id}>
+                                <th className="px-6 py-3 w-4">
+                                    <input
+                                        type="checkbox"
+                                        className="cursor-pointer rounded border-gray-300"
+                                        checked={table.getIsAllRowsSelected()}
+                                        onChange={table.getToggleAllRowsSelectedHandler()} // TanStack handles the logic
+                                    />
+                                </th>
                                 {headerGroup.headers.map((header) => (
                                     <th key={header.id} className="px-6 py-3 whitespace-nowrap">
-                                        {header.isPlaceholder
-                                            ? null
-                                            : flexRender(
-                                                header.column.columnDef.header,
-                                                header.getContext(),
-                                            )}
+                                        {header.isPlaceholder ? null : (
+                                            <div
+                                                className={`flex items-center text-center gap-0 ${header.column.getCanSort()
+                                                    ? "cursor-pointer select-none"
+                                                    : ""
+                                                    }`}
+                                                onClick={header.column.getToggleSortingHandler()}
+                                            >
+                                                <span className="mr-2">
+                                                    {/* 1. If Ascending, show Up */}
+                                                    {header.column.getCanSort() && header.column.getIsSorted() === 'asc' && <ChevronUp size={14} className="text-gray-500" />}
+
+                                                    {/* 2. If Descending, show Down */}
+                                                    {header.column.getCanSort() && header.column.getIsSorted() === 'desc' && <ChevronDown size={14} className="text-gray-500" />}
+
+                                                    {/* 3. If Not Sorted, show Default */}
+                                                    {header.column.getCanSort() && !header.column.getIsSorted() && <ChevronsUpDown size={14} className="text-gray-400" />}
+                                                </span>
+
+                                                {/* Header Text */}
+                                                {flexRender(
+                                                    header.column.columnDef.header,
+                                                    header.getContext()
+                                                )}
+                                            </div>
+                                        )}
                                     </th>
                                 ))}
                             </tr>
                         ))}
                     </thead>
-                    <tbody className="">
+                    <tbody className={isLoading ? "opacity-50 pointer-events-none" : ""}>
                         {table.getRowModel().rows.map((row) => (
                             <tr key={row.id} className="bg-white border-b hover:bg-gray-50">
+                                <td className="px-6 py-4 w-4">
+                                    <input
+                                        type="checkbox"
+                                        className="cursor-pointer rounded border-gray-300"
+                                        checked={row.getIsSelected()}
+                                        disabled={!row.getCanSelect()}
+                                        onChange={row.getToggleSelectedHandler()} // TanStack handles the logic
+                                    />
+                                </td>
                                 {row.getVisibleCells().map((cell) => (
                                     <td key={cell.id} className="px-6 py-4 whitespace-nowrap">
                                         {flexRender(cell.column.columnDef.cell, cell.getContext())}
